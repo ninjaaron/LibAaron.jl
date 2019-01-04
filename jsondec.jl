5615e5a408aee3ded70631f45a7cf3c35784cd48
Aaron Christianson <ninjaaron@gmail.com>
2019-01-04 23:13:30 +0100

    current

--- jsondec.jl
+++ jsondec.jl
@@ -7,20 +7,7 @@ string = raw"""
 "<datafield xmlns=\"http://viaf.org/viaf/terms#\" dtype=\"UNIMARC\" ind1=\" \" ind2=\" \" tag=\"215\">     <subfield code=\"7\">ba0yba0y</subfield> <subfield code=\"8\">fre   </subfield>     <subfield code=\"9\"> </subfield>     <subfield code=\"a\">Djelfa, Wilaya de (Alge\u0301rie)</subfield>   </datafield>"
 """[1:end-1]
 
-tobyte(c::Char) = convert(UInt8, c)
-macro mkbytes(args...)
-    iargs = Iterators.Stateful(args)
-    blk = quote end
-    for (sym, char) in zip(iargs, iargs)
-        push!(blk.args, :(const $sym = tobyte($char)))
-    end
-    blk
-end
-
-
-@mkbytes backslash '\\' slash '/' dq '"'
-@mkbytes nl '\n' bs '\b' ret '\r' tab '\t'
-@mkbytes n 'n' b 'b' r 'r' t 't' u 'u' 
+const ascii = [Char(1
 const utf8_buf = zeros(UInt8, 6)
 
 function string_unescape(str::String, buffer::Vector{UInt8})
