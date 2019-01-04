290679a44c6430839e0ccb4fb380b8d00baacca3
Aaron Christianson <ninjaaron@gmail.com>
2019-01-04 23:16:37 +0100

    current

--- jsondec.jl
+++ jsondec.jl
@@ -1,19 +1,18 @@
 #!/usr/bin/env julia
 using BenchmarkTools
 import JSON
-const Opt = Union{T,Nothing} where T
 
 string = raw"""
 "<datafield xmlns=\"http://viaf.org/viaf/terms#\" dtype=\"UNIMARC\" ind1=\" \" ind2=\" \" tag=\"215\">     <subfield code=\"7\">ba0yba0y</subfield> <subfield code=\"8\">fre   </subfield>     <subfield code=\"9\"> </subfield>     <subfield code=\"a\">Djelfa, Wilaya de (Alge\u0301rie)</subfield>   </datafield>"
 """[1:end-1]
 
-const ascii = [Char(1
+const ascii = [Char(i) for i in 1:127]
 const utf8_buf = zeros(UInt8, 6)
 
 function string_unescape(str::String, buffer::Vector{UInt8})
     bytes = Base.CodeUnits(str)
     len = length(bytes)
-    length(buffer) < len  && throw(BoundsError(buffer, length(bytes)))
+    length(buffer) * 2 < len  && throw(BoundsError(buffer, length(bytes)))
     byteidx::Int = buffidx::Int = 0
     @inbounds while (i += 1) <= bytes
         j += 1
